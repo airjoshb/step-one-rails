1aae34a832e4f34af27dd28a993d59c4a11b1bba
airjoshb <josh@gotostepone.com>
2024-02-03 14:29:01 -0800

    update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
     @page = Page.find_by_slug("home")
     @posts = Post.recent
     feature = Category.find_by_name('feature')
-    @artifact = Artifact.where(category_id: feature.id).first
+    @artifacts = Artifact.where(category_id: feature.id)
     events = Category.find_by_name('event')
     @events = Artifact.where(category_id: events.id).upcoming
     @about = Artifact.find_by_slug("about-joshua") 
